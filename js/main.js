93e9142e462446f36311addb8452a35fd1c7ea88
AugusteDollinger <adollinger@edenschool.fr>
2024-10-18 19:14:55 +0200

    added draggable windows

--- js/main.js
+++ js/main.js
@@ -7,10 +7,16 @@ $(document).on('mousemove', function(e) {
         });
     }, 50);
 });
+$('#about').draggable()
 $(document).ready(function () {
     $('#presentation').css({
         transform: 'translate(-50%, -50%)',
         left: '50%',
         top: '50%'
+    })
+    $('#about').css({
+        right: '70%',
+        bottom: '20%',
+        transition: 'right 2s ease, bottom 2s ease'
     })
 })
