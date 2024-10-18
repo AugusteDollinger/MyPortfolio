f2cfa05897ae4407495bf105c4810c3a8d6eb450
Auguste <adollinger@edenschool.fr>
2024-10-18 16:09:58 +0200

    .ready transition

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 let deplacement = []
 $(document).on('mousemove', function(e) {
-    $('#cursor').css({
-        left: e.pageX,
-        top: e.pageY
-    });
+    setTimeout(function() {
+        $('#cursor').css({
+            left: e.pageX,
+            top: e.pageY
+        });
+    }, 50);
 });
+$(document).ready(function () {
+    $('#presentation').css({
+        transform: 'translate(-50%, -50%)',
+        left: '50%',
+        top: '50%'
+    })
+})
